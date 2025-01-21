7cd3b3b16c856db3f376a816a732fbce405faa07
Tom Lamb <tmlamb@gmail.com>
2025-01-20 19:50:28 -0600

    expo 52 upgrade

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,42 +1,59 @@
 import type { ConfigContext, ExpoConfig } from "expo/config";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "../../.env" });
+
+const { APP_ENV, NEXTAUTH_URL, SENTRY_PUBLIC_DSN } = process.env;
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: "expo",
-  slug: "expo",
-  scheme: "expo",
-  version: "0.1.0",
+  name: `Probable Pitcher${APP_ENV !== "production" ? ` (${APP_ENV})` : ""}`,
+  slug: "probable-pitcher",
+  scheme: "com.probablepitcher",
+  owner: "tmlamb",
+  version: "2.0.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
   splash: {
-    image: "./assets/icon.png",
+    image: "./assets/splash.png",
     resizeMode: "contain",
-    backgroundColor: "#1F104A",
+    backgroundColor: "#fff",
   },
   updates: {
     fallbackToCacheTimeout: 0,
   },
   assetBundlePatterns: ["**/*"],
   ios: {
-    bundleIdentifier: "your.bundle.identifier",
+    bundleIdentifier: `com.probablepitcher${
+      APP_ENV !== "production" ? `.${APP_ENV}` : ""
+    }`,
+
     supportsTablet: true,
   },
   android: {
-    package: "your.bundle.identifier",
+    package: `com.probablepitcher${
+      APP_ENV !== "production" ? `.${APP_ENV}` : ""
+    }`,
+
     adaptiveIcon: {
-      foregroundImage: "./assets/icon.png",
-      backgroundColor: "#1F104A",
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#fff",
     },
+    googleServicesFile: "./google-services.json",
   },
-  // extra: {
-  //   eas: {
-  //     projectId: "your-eas-project-id",
-  //   },
-  // },
+  extra: {
+    eas: {
+      projectId: "abd1f73f-1027-4617-a091-abea678a0784",
+    },
+    apiBaseUrl: NEXTAUTH_URL,
+    nextAuthUrl: NEXTAUTH_URL,
+    sentryPublicDsn: SENTRY_PUBLIC_DSN,
+    appEnv: APP_ENV,
+  },
   experiments: {
     tsconfigPaths: true,
     typedRoutes: true,
   },
-  plugins: ["expo-router"],
+  plugins: ["expo-router", "sentry-expo", "expo-localization"],
 });
