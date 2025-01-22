cb32375287200f5a87a7c0cb2902380bca718834
Tom Lamb <tmlamb@gmail.com>
2025-01-22 16:40:04 -0600

    fix preview

--- tests/integration/vitest.config.ts
+++ tests/integration/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     env: {
-      VITE_API_URL: "http://nextjs:3000",
+      VITE_API_URL: "http://nextjs.:3000",
     },
   },
 });
