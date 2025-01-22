f98f63e3647f9503d3897495e019ed4e9573d59d
Tom Lamb <tmlamb@gmail.com>
2025-01-22 10:32:13 -0600

    debug auth proxy

--- infrastructure/src/index.ts
+++ infrastructure/src/index.ts
@@ -767,7 +767,7 @@ const loginDeployment = new k8s.apps.v1.Deployment(
                 },
               },
               livenessProbe: {
-                httpGet: { path: "/healthcheck", port: 3012 },
+                httpGet: { path: "/healthcheck", port: "http" },
                 initialDelaySeconds: 10,
                 timeoutSeconds: 5,
               },
