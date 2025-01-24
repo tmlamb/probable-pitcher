66af3f9ae710d8e307c51bf7a4c38ea8db3b1cc2
Tom Lamb <tmlamb@gmail.com>
2025-01-24 07:23:45 -0600

    debug apple provider

--- apps/auth-proxy/routes/r/[...auth].ts
+++ apps/auth-proxy/routes/r/[...auth].ts
@@ -8,6 +8,18 @@ export default eventHandler(async (event) =>
     basePath: "/r",
     secret: process.env.AUTH_SECRET,
     redirectProxyUrl: process.env.AUTH_REDIRECT_PROXY_URL,
+    debug: true,
+    cookies: {
+      pkceCodeVerifier: {
+        name: "next-auth.pkce.code_verifier",
+        options: {
+          httpOnly: true,
+          sameSite: "none",
+          path: "/",
+          secure: true,
+        },
+      },
+    },
     providers: [
       Google({
         clientId: process.env.AUTH_GOOGLE_ID,
@@ -16,6 +28,23 @@ export default eventHandler(async (event) =>
       Apple({
         clientId: process.env.AUTH_APPLE_ID,
         clientSecret: process.env.AUTH_APPLE_SECRET ?? "uh-oh",
+        wellKnown: "https://appleid.apple.com/.well-known/openid-configuration",
+        checks: ["pkce"],
+        token: {
+          url: `https://appleid.apple.com/auth/token`,
+        },
+        authorization: {
+          url: "https://appleid.apple.com/auth/authorize",
+          params: {
+            scope: "",
+            response_type: "code",
+            response_mode: "query",
+            state: crypto.randomUUID(),
+          },
+        },
+        client: {
+          token_endpoint_auth_method: "client_secret_post",
+        },
       }),
     ],
     trustHost: true,
