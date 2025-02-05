0afdb62ec35e6d1f92cb1ff4b7d10fbdbe9f4321
Tom Lamb <tmlamb@gmail.com>
2025-02-05 11:43:30 -0600

    better auth poc

--- apps/auth-proxy/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import baseConfig from "@probable/eslint-config/base";
-
-/** @type {import('typescript-eslint').Config} */
-export default [
-  {
-    ignores: [".nitro/**", ".output/**"],
-  },
-  ...baseConfig,
-];
