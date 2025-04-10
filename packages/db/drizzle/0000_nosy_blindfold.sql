85b16eb78a65a5f89ec67f07a48f869650dd050c
Tom Lamb <tmlamb@gmail.com>
2025-04-09 21:10:35 -0500

    fix preview builds

--- packages/db/drizzle/0000_nosy_blindfold.sql
+++ packages/db/drizzle/0000_nosy_blindfold.sql
@@ -1,4 +1,2 @@
--- Custom SQL migration file, put you code below! --
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
--- CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
 CREATE INDEX "name_search_index" ON "pitcher" USING gin ("name" gin_trgm_ops);
