cb07f2cf095579e674b0a6cad30b91563eaeebf7
Tom Lamb <tmlamb@gmail.com>
2025-05-13 15:50:32 -0500

    Revert "fix lint"
    This reverts commit 5e7f28cafbe01655e8f24b5044bc64409c93766d.

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 
 import { HydrateClient } from "~/trpc/server";
 import AppleClientSignin from "./_components/apple-signin";
+import { AuthShowcase } from "./_components/auth-showcase";
 import {
   SubscriptionList,
   //CreatePostForm,
