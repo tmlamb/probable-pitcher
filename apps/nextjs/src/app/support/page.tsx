5b96b9202ba9ec9914baebd9bc25a44ff2e9b45d
Tom Lamb <tmlamb@gmail.com>
2025-05-14 20:39:48 -0500

    support page

---
+++ apps/nextjs/src/app/support/page.tsx
@@ -0,0 +1,45 @@
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+
+import { faqs } from "@probable/ui";
+import { Button } from "@probable/ui/button";
+
+export default function Support() {
+  return (
+    <main className="container mx-auto flex min-h-screen max-w-7xl flex-col items-start justify-start space-y-4 px-3 py-8 sm:px-8">
+      <h1 className="text-3xl font-extrabold leading-normal text-gray-700 sm:text-5xl">
+        Support
+      </h1>
+      <div className="flex max-w-prose flex-col space-y-4">
+        <p>
+          Thank you for using Probable Pitcher! This service is developed as an
+          open source project on the GitHub platform. If you have any questions
+          or need help, please reach out over GitHub:
+        </p>
+        <Button asChild>
+          <a href="https://github.com/tmlamb/probable-pitchers/issues/new">
+            <GitHubLogoIcon
+              className="mr-2 inline h-6 w-6"
+              aria-hidden="true"
+            />
+            Report Issue On Github
+          </a>
+        </Button>
+        <h2 className="pt-4 text-2xl">FAQs</h2>
+        {faqs.map((faq) => (
+          <div key={faq.question}>
+            <h3 className="pt-2 font-bold">{faq.question}</h3>
+            <p>{faq.answer}</p>
+          </div>
+        ))}
+        <h2 className="pt-4 text-2xl">Contact Us</h2>
+        <p>
+          The fastest way to receive support is to open an issue on the GitHub
+          page linked above. Emails are also welcome:
+          <span className="block pt-2 text-sm">
+            contact@probablepitcher.com
+          </span>
+        </p>
+      </div>
+    </main>
+  );
+}
