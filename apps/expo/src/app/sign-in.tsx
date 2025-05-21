1a7e044ccec29ee919692cf3d139e36b74f93a63
Tom Lamb <tmlamb@gmail.com>
2025-05-21 12:30:08 -0500

    disable refresh

--- apps/expo/src/app/sign-in.tsx
+++ apps/expo/src/app/sign-in.tsx
@@ -37,8 +37,6 @@ export default function SignIn() {
     function checkAuth() {
       if (session.data) {
         router.replace("/");
-      } else {
-        queryClient.invalidateQueries(trpc.pathFilter()).catch(console.error);
       }
     }
 
