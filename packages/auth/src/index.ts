e855a6a68d5a7be1a439443b0a1c9b918848400f
Tom Lamb <tmlamb@gmail.com>
2025-06-16 22:59:11 -0500

    Reapply "Reapply "Reapply "web app wip"""
    This reverts commit 715113f6af18bdd240a4472c31010e5cabe9cc25.

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,10 +1,52 @@
-import { headers } from "next/headers";
+import { expo } from "@better-auth/expo";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { apiKey } from "better-auth/plugins";
 
-import { auth } from "./auth";
+import { db } from "@probable/db/client";
 
-export const getSession = async () =>
-  auth.api.getSession({
-    headers: await headers(),
-  });
+import { env } from "../env";
 
-export * from "./auth";
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  plugins: [expo(), apiKey()],
+  emailAndPassword: {
+    enabled: false,
+  },
+  socialProviders: {
+    google: {
+      clientId: env.AUTH_GOOGLE_ID,
+      clientSecret: env.AUTH_GOOGLE_SECRET,
+    },
+    apple: {
+      clientId: env.AUTH_APPLE_SERVICE_ID,
+      clientSecret: env.AUTH_APPLE_SECRET,
+      appBundleIdentifier: env.AUTH_APPLE_BUNDLE_ID,
+    },
+  },
+  trustedOrigins: [
+    "probablepitcher://",
+    "https://dev.probablepitcher.com",
+    "https://probablepitcher.com",
+    "http://localhost:3000",
+    "https://appleid.apple.com",
+  ],
+  logger: {
+    level: "debug",
+    disabled: true,
+  },
+  advanced: {
+    cookiePrefix: "probable-pitcher",
+  },
+  account: {
+    accountLinking: {
+      enabled: true,
+      trustedProviders: ["google", "apple"],
+    },
+  },
+});
+
+export type Session = typeof auth.$Infer.Session;
+export type Auth = typeof auth;
