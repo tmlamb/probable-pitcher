6e939a5c7dbb3b8d0130048a2944476d9dc6a781
Tom Lamb <tmlamb@gmail.com>
2025-06-16 21:49:49 -0500

    Revert "Reapply "web app wip""
    This reverts commit baeaf0e5fc4d0a36c06c5904cc55ffbdcc9e4a5e.

--- apps/nextjs/src/app/privacy/page.tsx
+++ apps/nextjs/src/app/privacy/page.tsx
@@ -4,163 +4,102 @@ export default function Privacy() {
       <h1 className="text-3xl font-extrabold leading-normal text-gray-700 sm:text-5xl">
         Privacy Policy
       </h1>
-      <div className="flex max-w-prose flex-col space-y-4 text-gray-800">
-        <p className="text-sm text-gray-500">Effective Date: June 9, 2025</p>
-
-        <h2 className="pt-4 text-2xl font-bold">1. Introduction</h2>
+      <div className="flex max-w-prose flex-col space-y-4">
         <p>
-          Welcome to Probable Pitcher. This Privacy Policy explains how we
-          collect, use, and protect your information when you use our iOS and
-          Android mobile applications and our website located at
-          https://probablepitcher.com (collectively, the &quot;Service&quot;).
+          Probable Pitcher operates the iOS and Android mobile apps and
+          https://probablepitcher.com website, which provide the SERVICE.
         </p>
         <p>
-          By using our Service, you agree to the collection and use of
-          information in accordance with this policy.
+          This page is used to inform mobile app users and website visitors
+          regarding our policies with the collection, use, and disclosure of
+          Personal Information if anyone decided to use our Service, the
+          Probable Pitcher website and mobile app.
         </p>
-
-        <h2 className="pt-4 text-2xl font-bold">2. Information We Collect</h2>
         <p>
-          We collect information to provide and improve our Service to you. The
-          types of information we collect are:
+          If you choose to use our Service, then you agree to the collection and
+          use of information in relation with this policy. The Personal
+          Information that we collect are used for providing and improving the
+          Service. We will not use or share your information with anyone.
         </p>
-        <h3 className="pt-2 text-xl font-semibold">
-          a. Information You Provide to Us
-        </h3>
+        <h3 className="pt-4 text-2xl">Information Collection and Use</h3>
         <p>
-          <strong>Personally Identifiable Information:</strong> When you use our
-          Service, we may ask you to provide us with certain personally
-          identifiable information that can be used to contact or identify you.
-          This may include, but is not limited to, your email address.
+          For a better experience while using our Service, we may require you to
+          provide us with certain personally identifiable information, including
+          your email address. The information that we collect will be used to
+          contact or identify you.
         </p>
-        <h3 className="pt-2 text-xl font-semibold">
-          b. Information We Collect Automatically (Log Data)
-        </h3>
+        <h3 className="pt-4 text-2xl">Log Data</h3>
         <p>
-          <strong>Usage Details:</strong> When you access the Service, we
-          automatically collect certain information sent by your device or web
-          browser. This &quot;Log Data&quot; may include your device&apos;s
-          Internet Protocol (&quot;IP&quot;) address, device type, operating
-          system, browser type and version, the pages of our Service that you
-          visit, the time and date of your visit, the time spent on those pages,
-          and other diagnostic statistics.
+          We want to inform you that whenever you visit our Service, we collect
+          information that your browser or device sends to us that is called Log
+          Data. This Log Data may include information such as your
+          computer&apos;s Internet Protocol (&quot;IP&quot;) address, browser
+          version, pages of our Service that you visit, the time and date of
+          your visit, the time spent on those pages, and other statistics.
         </p>
-
-        <h2 className="pt-4 text-2xl font-bold">
-          3. How We Use Your Information
-        </h2>
+        <h3 className="pt-4 text-2xl">Cookies </h3>
         <p>
-          We use the information we collect for various purposes, including:
+          Cookies are files with a small amount of data that is commonly used as
+          an anonymous unique identifier. These are sent to your browser or
+          device from the website or application service backend that you visit
+          and are stored on your computer&apos;s hard drive.
         </p>
-        <ul className="ml-8 list-disc space-y-1">
-          <li>To provide, maintain, and operate our Service.</li>
-          <li>To improve, personalize, and expand our Service.</li>
-          <li>To understand and analyze how you use our Service.</li>
-          <li>
-            To communicate with you, either directly or through one of our
-            partners, including for customer service and to provide you with
-            updates and other information relating to the Service.
-          </li>
-          <li>To identify and prevent fraud.</li>
-        </ul>
-
-        <h2 className="pt-4 text-2xl font-bold">
-          4. How We Share Your Information
-        </h2>
         <p>
-          We do not share your Personal Information with anyone except as
-          described in this Privacy Policy. We may employ third-party companies
-          and individuals (&quot;Service Providers&quot;) for the following
-          reasons:
+          Our service uses these &quot;cookies&quot; to collect information and
+          to improve our Service. You have the option to either accept or refuse
+          these cookies, and know when a cookie is being sent to your computer.
+          If you choose to refuse our cookies, you may not be able to use some
+          portions of our Service.
         </p>
-        <ul className="ml-8 list-disc space-y-1">
+        <h3 className="pt-4 text-2xl">Service Providers</h3>
+        <p>
+          We may employ third-party companies and individuals due to the
+          following reasons:
+        </p>
+        <ul className="ml-8 list-disc">
           <li>To facilitate our Service;</li>
           <li>To provide the Service on our behalf;</li>
-          <li>
-            To perform services related to the Service (e.g., maintenance,
-            database management);
-          </li>
+          <li>To perform Service-related services; or</li>
           <li>To assist us in analyzing how our Service is used.</li>
         </ul>
         <p>
-          These third parties have access to your Personal Information only to
-          perform these tasks on our behalf and are obligated not to disclose or
-          use it for any other purpose.
+          We want to inform our Service users that these third parties have
+          access to your Personal Information. The reason is to perform the
+          tasks assigned to them on our behalf. However, they are obligated not
+          to disclose or use the information for any other purpose.
         </p>
-
-        <h2 className="pt-4 text-2xl font-bold">
-          5. Cookies and Tracking Technologies
-        </h2>
+        <h3 className="pt-4 text-2xl">Security</h3>
         <p>
-          We use cookies and similar tracking technologies to track activity on
-          our Service and hold certain information.
+          We value your trust in providing us your Personal Information, thus we
+          are striving to use commercially acceptable means of protecting it.
+          But remember that no method of transmission over the internet, or
+          method of electronic storage is 100% secure and reliable, and we
+          cannot guarantee its absolute security.
         </p>
+        <h3 className="pt-4 text-2xl">Links to Other Sites</h3>
         <p>
-          Cookies are files with a small amount of data which may include an
-          anonymous unique identifier. They are sent to your browser from a
-          website and stored on your device. You have the option to either
-          accept or refuse these cookies and to know when a cookie is being sent
-          to your device. If you choose to refuse our cookies, you may not be
-          able to use some portions of our Service.
+          Our Service may contain links to other sites. If you click on a
+          third-party link, you will be directed to that site. Note that these
+          external sites are not operated by us. Therefore, we strongly advise
+          you to review the Privacy Policy of these websites. We have no control
+          over, and assume no responsibility for the content, privacy policies,
+          or practices of any third-party sites or services.{" "}
         </p>
-
-        <h2 className="pt-4 text-2xl font-bold">6. Data Security</h2>
+        <h3 className="pt-4 text-2xl">Changes to This Privacy Policy</h3>
         <p>
-          We value your trust and take the security of your data seriously. We
-          use commercially acceptable means to protect your Personal
-          Information. However, please remember that no method of transmission
-          over the internet or method of electronic storage is 100% secure and
-          reliable. While we strive to protect your data, we cannot guarantee
-          its absolute security.
+          We may update our Privacy Policy from time to time. Thus, we advise
+          you to review this page periodically for any changes. We will notify
+          you of any changes by posting the new Privacy Policy on this page.
+          These changes are effective immediately, after they are posted on this
+          page.
         </p>
-
-        <h2 className="pt-4 text-2xl font-bold">7. Links to Other Sites</h2>
+        <h3 className="pt-4 text-2xl">Contact Us</h3>
         <p>
-          Our Service may contain links to other sites that are not operated by
-          us. If you click on a third-party link, you will be directed to that
-          site. We strongly advise you to review the Privacy Policy of every
-          site you visit. We have no control over and assume no responsibility
-          for the content, privacy policies, or practices of any third-party
-          sites or services.
-        </p>
-
-        <h2 className="pt-4 text-2xl font-bold">8. Children&apos;s Privacy</h2>
-        <p>
-          Our Service is not intended for use by anyone under the age of 13. We
-          do not knowingly collect personally identifiable information from
-          children under 13. If you are a parent or guardian and you are aware
-          that your child has provided us with Personal Information, please
-          contact us. If we become aware that we have collected Personal
-          Information from a child under 13 without verification of parental
-          consent, we will take steps to remove that information from our -
-          servers.
-        </p>
-
-        <h2 className="pt-4 text-2xl font-bold">
-          9. Changes to This Privacy Policy
-        </h2>
-        <p>
-          We may update our Privacy Policy from time to time. We will notify you
-          of any changes by posting the new Privacy Policy on this page. You are
-          advised to review this Privacy Policy periodically for any changes.
-          Changes to this Privacy Policy are effective when they are posted on
-          this page.
-        </p>
-        <p>
-          For any significant changes, we may provide a more prominent notice
-          (such as an in-app notification or an email).
-        </p>
-
-        <h2 className="pt-4 text-2xl font-bold">10. Contact Us</h2>
-        <p>
-          If you have any questions or suggestions about our Privacy Policy,
-          please do not hesitate to contact us at:
-          <a
-            href="mailto:contact@probablepitcher.com"
-            className="mt-2 block text-blue-600 hover:underline"
-          >
+          If you have any questions or suggestions about our Privacy Policy, do
+          not hesitate to contact us:
+          <span className="block pt-2 text-sm">
             contact@probablepitcher.com
-          </a>
+          </span>
         </p>
       </div>
     </main>
