f8bb8787902b000ee43015b6c23d7befaa0e2bac
Tom Lamb <tmlamb@gmail.com>
2025-10-07 20:27:36 -0500

    test deploy

--- infrastructure/src/index.ts
+++ infrastructure/src/index.ts
@@ -661,7 +661,7 @@ const appDeployment = new k8s.apps.v1.Deployment(
               resources: {
                 requests: {
                   cpu: isProd ? "25m" : "5m",
-                  memory: isProd ? "256Mi" : "128Mi",
+                  memory: isProd ? "256Mi" : "56Mi",
                 },
                 limits: {
                   cpu: isProd ? "100m" : "50m",
