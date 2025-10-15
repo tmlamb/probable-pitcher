c5f8996b43a380504bc5f59a7d7ffa756b00bf8e
Tom Lamb <tmlamb@gmail.com>
2025-10-14 20:52:30 -0500

    debug auth proxy

--- apps/nextjs/src/app/api/auth/[...all]/route.ts
+++ apps/nextjs/src/app/api/auth/[...all]/route.ts
@@ -1,5 +1,4 @@
-import { toNextJsHandler } from "better-auth/next-js";
+import { auth } from "~/auth/server";
 
-import { auth } from "@probable/auth";
-
-export const { GET, POST } = toNextJsHandler(auth.handler);
+export const GET = auth.handler;
+export const POST = auth.handler;
--- apps/nextjs/src/env.ts
+++ apps/nextjs/src/env.ts
@@ -1,10 +1,10 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod/v4";
 
-import { env as authEnv } from "@probable/auth/env";
+import { authEnv } from "@probable/auth/env";
 
 export const env = createEnv({
-  extends: [authEnv],
+  extends: [authEnv()],
   shared: {
     NODE_ENV: z
       .enum(["development", "production", "test"])
@@ -29,11 +29,8 @@ export const env = createEnv({
    * Destructure all variables from `process.env` to make sure they aren't
    * tree-shaken away.
    */
-  runtimeEnv: {
+  experimental__runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    DATABASE_URL: process.env.DATABASE_URL,
-
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   skipValidation:
     !!process.env.CI || process.env.npm_lifecycle_event === "lint",
