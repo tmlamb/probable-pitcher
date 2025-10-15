226977e4c7fb88120dff0fcf708ccfcc970afbef
Tom Lamb <tmlamb@gmail.com>
2025-10-15 17:18:12 -0500

    debug auth proxy

--- packages/auth/env.ts
+++ packages/auth/env.ts
@@ -13,16 +13,7 @@ export function authEnv() {
       BETTER_AUTH_URL: z.string().min(1),
       NODE_ENV: z.enum(["development", "production"]).optional(),
     },
-    runtimeEnv: {
-      AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
-      AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
-      AUTH_APPLE_BUNDLE_ID: process.env.AUTH_APPLE_BUNDLE_ID,
-      AUTH_APPLE_SERVICE_ID: process.env.AUTH_APPLE_SERVICE_ID,
-      AUTH_APPLE_SECRET: process.env.AUTH_APPLE_SECRET,
-      BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
-      BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
-      NODE_ENV: process.env.NODE_ENV,
-    },
+    experimental__runtimeEnv: {},
     skipValidation:
       !!process.env.CI || process.env.npm_lifecycle_event === "lint",
   });
