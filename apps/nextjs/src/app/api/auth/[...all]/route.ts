d73e95d5c86f14900f70f90014b9fd8f6ebed232
Tom Lamb <tmlamb@gmail.com>
2025-10-21 14:32:01 -0500

    Reapply "debug auth proxy"
    This reverts commit 5ebe0876141b6c83362bac71d7df17ac66b06f1b.

--- apps/nextjs/src/app/api/auth/[...all]/route.ts
+++ apps/nextjs/src/app/api/auth/[...all]/route.ts
@@ -1,4 +1,5 @@
+import { toNextJsHandler } from "better-auth/next-js";
+
 import { auth } from "~/auth/server";
 
-export const GET = auth.handler;
-export const POST = auth.handler;
+export const { GET, POST } = toNextJsHandler(auth);
