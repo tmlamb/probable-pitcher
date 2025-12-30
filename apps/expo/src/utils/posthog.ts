00502491023dfea6655987b9f3f1cac4c70bb655
Tom Lamb <tmlamb@gmail.com>
2025-12-30 16:40:11 -0600

    posthog wip

--- apps/expo/src/utils/posthog.ts
+++ apps/expo/src/utils/posthog.ts
@@ -1,9 +1,10 @@
+import Constants from "expo-constants";
 import PostHog from "posthog-react-native";
 
 import { getBaseUrl } from "./base-url";
 
 const { posthogApiKey } = Constants.expoConfig?.extra ?? {};
 
-export const posthog = new PostHog(posthogApiKey, {
+export const posthog = new PostHog(String(posthogApiKey), {
   host: `${getBaseUrl()}/pt`,
 });
