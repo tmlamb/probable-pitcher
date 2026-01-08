148c4fc1281e6cea3eaf82ec884357bc89c339f6
Tom Lamb <tmlamb@gmail.com>
2026-01-07 19:39:46 -0600

    debug docker build

--- apps/nextjs/src/instrumentation-client.ts
+++ apps/nextjs/src/instrumentation-client.ts
@@ -2,6 +2,8 @@ import posthog from "posthog-js";
 
 import { env } from "./env";
 
+console.log("PostHog API Key:", env.NEXT_PUBLIC_POSTHOG_API_KEY);
+
 if (env.NEXT_PUBLIC_POSTHOG_API_KEY) {
   posthog.init(env.NEXT_PUBLIC_POSTHOG_API_KEY, {
     api_host: "/pt",
